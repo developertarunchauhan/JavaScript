b52858d5c7cc11131c38a74dfec82b4a617732bd
developertarunchauhan <developer.tarunchauhan@gmail.com>
2023-05-08 11:58:37 +0530

    JS Basics

---
+++ js/script.js
@@ -0,0 +1,41 @@
+console.log("JavaScript Basic to Advance"); // returns data to console
+
+/**
+ * JavaScript Data types : 
+ * 1. String
+ * 2. Number
+ * 3. Bigint
+ * 4. Boolean
+ * 5. Undefined
+ * 6. Null
+ * 7. Symbol
+ * 8. Object
+ */
+
+/**
+ *  Declaring a variable :
+ */
+
+var name = "Tarun Chauhan"; // can be accessed anywhere in a program
+
+let job = "Software Developer"; // can be accessed inside a scope where it is declared only
+
+const YOB = 1990; // immuatable value
+
+
+/**
+ * Assignment Variables : 
+ */
+
+var a;
+a = 2;
+var b = 3; // `=` is an assignment operator
+
+/**
+ * Uninitialized variables : variable is declared but value not set
+ */
+
+var aa, bb, cc;
+
+
+
