fed60400179cb03e9ee889da8cc379cdcd76d9e3
developertarunchauhan <developer.tarunchauhan@gmail.com>
2023-05-09 10:27:23 +0530

    JS ARRAY

--- js/script.js
+++ js/script.js
@@ -38,4 +38,64 @@ var b = 3; // `=` is an assignment operator
 var aa, bb, cc;
 
 
+var msg = "Hello, my name is " + name + "and I am well";
 
+console.log(msg);
+console.log(msg.length);
+
+name[0] = "V"; // can't be done
+
+name = "Varun Chauhan"; // we can change entire value
+
+console.log(name);
+
+/**
+ * 
+ * Array : Special variable that can hold more than one data of same or different data type.
+ * 
+ */
+
+
+var myList = ["Tarun Chauhan", 32, "Software Developer"];
+
+console.log(myList);
+
+console.log(myList[2]);
+
+myList[1] = 33;
+
+console.log(myList);
+
+
+/**
+ * Multidimensional array : Array inside and another array
+ */
+
+var mulArray = [
+    ["Tarun Chauhan", 32, "Software Developer"],
+    ["Priya Chauhan", 28, "House Wife"],
+    ["Yagnya Chauhan", 1, "Baby"]
+];
+
+console.log(mulArray);
+
+console.log(mulArray[1]);
+
+console.log(mulArray[1][2]);
+
+mulArray[2][2] = "Toodler";
+
+console.log(mulArray[2]);
+
+
+/**
+ * 3 - level array : 
+ */
+
+var threeLevel = [
+    ["abc", 123],
+    ["pqr", 789],
+    [["x", "y", "z"], 5467]
+];
+
+console.log(threeLevel);
